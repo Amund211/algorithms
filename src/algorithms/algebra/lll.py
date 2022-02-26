a1136c2a0e594eeab8f6a725c42ef5962670a516
Amund Eggen Svandal <aesvandal@gmail.com>
2022-02-26 19:02:23 +0100

    Add the LLL algorithm

--- src/algorithms/algebra/lll.py
+++ src/algorithms/algebra/lll.py
@@ -18,18 +18,60 @@ class Vector(tuple[float, ...]):
 def gram_schmidt(
     basis: tuple[Vector, ...]
 ) -> tuple[tuple[Vector, ...], list[list[float]]]:
+    """
+    Compute the non-normalized Gram Schmidt basis
+
+    https://en.wikipedia.org/wiki/Gram%E2%80%93Schmidt_process#Algorithm
+    """
     if not basis:
         return (), [[]]
 
     dimension = len(basis)
     assert all(len(vector) == dimension for vector in basis)
 
-    new_basis: list[Vector] = [basis[0]]
+    gram_basis: list[Vector] = [basis[0]]
     Q = [[0.0] * len(basis) for _ in range(len(basis))]
     for i in range(1, dimension):
-        new_basis.append(basis[i])
+        gram_basis.append(basis[i])
         for j in range(0, i):
-            Q[i][j] = (new_basis[j] @ new_basis[i]) / (new_basis[j] @ new_basis[j])
-            new_basis[i] -= new_basis[j].multiply(Q[i][j])
+            Q[i][j] = (basis[i] @ gram_basis[j]) / (gram_basis[j] @ gram_basis[j])
+            gram_basis[i] -= gram_basis[j].multiply(Q[i][j])
 
-    return tuple(new_basis), Q
+    return tuple(gram_basis), Q
+
+
+def lll(basis: tuple[Vector, ...], delta: float = 3 / 4) -> tuple[Vector, ...]:
+    """
+    Compute the LLL reduced basis
+
+    https://en.wikipedia.org/wiki/Lenstra%E2%80%93Lenstra%E2%80%93Lov%C3%A1sz_lattice_basis_reduction_algorithm#LLL_algorithm_pseudocode  # noqa: E501
+    """
+    n = len(basis)
+    k = 1
+
+    reduced = list(basis)
+
+    gram_basis, Q = gram_schmidt(tuple(reduced))
+
+    while k < n:
+        for j in range(k - 1, -1, -1):
+            if abs(Q[k][j]) > 1 / 2:
+                reduced[k] -= reduced[j].multiply(round(Q[k][j]))
+
+                # TODO: recompute only as necessary
+                gram_basis, Q = gram_schmidt(tuple(reduced))
+
+        if gram_basis[k] @ gram_basis[k] >= (delta - Q[k][k - 1] ** 2) * (
+            gram_basis[k - 1] @ gram_basis[k - 1]
+        ):
+            k += 1
+        else:
+            # Swap
+            reduced[k], reduced[k - 1] = reduced[k - 1], reduced[k]
+
+            # TODO: recompute only as necessary
+            gram_basis, Q = gram_schmidt(tuple(reduced))
+
+            k = max(k - 1, 1)
+
+    return tuple(reduced)
--- tests/algorithms/algebra/test_lll.py
+++ tests/algorithms/algebra/test_lll.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pytest
 
-from algorithms.algebra.lll import Vector, gram_schmidt
+from algorithms.algebra.lll import Vector, gram_schmidt, lll
 
 GRAM_SCHMIDT_CASES = (
     ((Vector([3, 1]), Vector([2, 2])), (Vector([3, 1]), Vector([-2 / 5, 6 / 5]))),
@@ -12,3 +12,19 @@ GRAM_SCHMIDT_CASES = (
 def test_gram_schmidt(basis: tuple[Vector, ...], new_basis: tuple[Vector, ...]) -> None:
     for vector, new_vector in zip(gram_schmidt(basis)[0], new_basis, strict=True):
         assert np.allclose(vector, new_vector)
+
+
+LLL_CASES = (
+    (
+        (Vector([1, 1, 1]), Vector([-1, 0, 2]), Vector([3, 5, 6])),
+        3 / 4,
+        (Vector([0, 1, 0]), Vector([1, 0, 1]), Vector([-1, 0, 2])),
+    ),
+)
+
+
+@pytest.mark.parametrize("basis, delta, new_basis", LLL_CASES)
+def test_lll(
+    basis: tuple[Vector, ...], delta: float, new_basis: tuple[Vector, ...]
+) -> None:
+    assert lll(basis, delta=delta) == new_basis
