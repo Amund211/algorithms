7336aefebfc44e50d3bdc32b985650bec49c4300
Amund Eggen Svandal <aesvandal@gmail.com>
2022-03-07 20:50:19 +0100

    Add row reduce mod 2

---
+++ src/algorithms/linear_algebra/row_reduce.py
@@ -0,0 +1,45 @@
+import numpy as np
+import numpy.typing as npt
+
+
+def row_reduce_mod_2(matrix: npt.NDArray[np.int32]) -> npt.NDArray[np.int32]:
+    """Reduce the given matrix over Z_2 to echelon form in place"""
+    assert len(matrix.shape) == 2
+    non_pivot_columns: list[int] = []
+
+    # Index where the next source row will be moved
+    free_row = 0
+
+    for column in range(matrix.shape[1]):
+        try:
+            source_row = next(
+                i for i in range(free_row, matrix.shape[0]) if matrix[i, column] == 1
+            )
+        except StopIteration:
+            # No rows with non-zero entries in this column - continue
+            continue
+
+        # Swap the new source row to the top
+        if source_row != free_row:
+            matrix[[source_row, free_row], :] = matrix[[free_row, source_row], :]
+
+        non_pivot_columns.append(column)
+
+        for other_row in range(free_row + 1, matrix.shape[0]):
+            # Eliminate this column from this row
+            if matrix[other_row, column] == 1:
+                matrix[other_row, :] += matrix[source_row, :]
+                matrix[other_row, :] %= 2
+
+        free_row += 1
+
+    for column in filter(lambda col: col != 0, non_pivot_columns):
+        source_row = next(i for i in range(column, -1, -1) if matrix[i, column] == 1)
+
+        for other_row in range(source_row):
+            # Eliminate this column from this row
+            if matrix[other_row, column] == 1:
+                matrix[other_row, :] += matrix[source_row, :]
+                matrix[other_row, :] %= 2
+
+    return matrix
---
+++ tests/algorithms/linear_algebra/test_row_reduce.py
@@ -0,0 +1,25 @@
+import functools
+
+import numpy as np
+import numpy.typing as npt
+import pytest
+
+from algorithms.linear_algebra.row_reduce import row_reduce_mod_2
+
+A = functools.partial(np.array, dtype=np.int32)
+
+ROW_REDUCE_MOD_2_CASES = (
+    # Square cases
+    (A([[1, 1], [1, 1]]), A([[1, 1], [0, 0]])),
+    (A([[1, 1], [1, 0]]), A([[1, 0], [0, 1]])),
+    # Rectangular cases
+    (A([[1], [1]]), A([[1], [0]])),
+    (A([[1, 1, 1], [1, 0, 1]]), A([[1, 0, 1], [0, 1, 0]])),
+)
+
+
+@pytest.mark.parametrize("matrix, result", ROW_REDUCE_MOD_2_CASES)
+def test_row_reduce_mod_2(
+    matrix: npt.NDArray[np.int32], result: npt.NDArray[np.int32]
+) -> None:
+    assert (row_reduce_mod_2(matrix.copy()) == result).all()
