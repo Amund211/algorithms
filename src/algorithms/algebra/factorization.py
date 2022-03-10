c5ff76a6c5ab4ccb43b2cd637fdb802e3bd32742
Amund Eggen Svandal <aesvandal@gmail.com>
2022-03-10 15:46:50 +0100

    Factor out smooth_factor logic

--- src/algorithms/algebra/factorization.py
+++ src/algorithms/algebra/factorization.py
@@ -19,6 +19,19 @@ def product_mod_n(iterable: Iterable[int], n: int, *, initial: int = 1) -> int:
 def index_calculus(n: int, primes: tuple[int, ...]) -> tuple[int, int]:
     """Compute the factorization of n=pq using index calculus"""
 
+    def suggest_r(n: int) -> Iterable[int]:
+        """Randomly sample integers in Z_n"""
+        while True:
+            yield random.randrange(1, n)
+
+    return smooth_factor(n, suggest_r(n), primes)
+
+
+def smooth_factor(
+    n: int, r_suggestions: Iterable[int], primes: tuple[int, ...]
+) -> tuple[int, int]:
+    """Compute the factorization of n=pq using smooth numbers"""
+
     L = len(primes)
 
     # Sanity checks
@@ -26,85 +39,91 @@ def index_calculus(n: int, primes: tuple[int, ...]) -> tuple[int, int]:
     assert n > L
     assert n > max(primes) ** 2
 
-    # List of all the rs we have found that factors into `primes`
-    r_list: list[int] = [-1] * (L + 1)
-    # The power of `primes` in each r. One column per r.
-    powers_matrix: npt.NDArray[np.int32] = np.empty((L, L + 1), dtype=np.int32)
+    while True:
+        # Restart the iterable to get new rs
+        r_iterator = iter(r_suggestions)
 
-    r_index = 0
-    seen_rs: set[int] = set()
+        # List of all the rs we have found that factors into `primes`
+        r_list: list[int] = [-1] * (L + 1)
+        # The power of `primes` in each r. One column per r.
+        powers_matrix: npt.NDArray[np.int32] = np.empty((L, L + 1), dtype=np.int32)
 
-    # Look for rs until we have L+1
-    while r_index < L + 1:
-        r = random.randrange(1, n)
+        r_index = 0
+        seen_rs: set[int] = set()
 
-        # Only consider unique rs
-        if r in seen_rs:
-            if len(seen_rs) > n / 2:
-                raise RuntimeError(f"Exhausted half of all possible numbers mod {n=}")
+        # Look for rs until we have L+1
+        while r_index < L + 1:
+            r = next(r_iterator)
+
+            # Only consider unique rs
+            if r in seen_rs:
+                if len(seen_rs) > n / 2:
+                    raise RuntimeError(
+                        f"Exhausted half of all possible numbers mod {n=}"
+                    )
+                continue
+
+            seen_rs.add(r)
+
+            try:
+                powers = factor_into(pow(r, 2, n), primes)
+            except FactorizationError:
+                # r^2 doesn't factor into `primes` - try again
+                continue
+
+            r_list[r_index] = r
+            powers_matrix[:, r_index] = powers
+
+            r_index += 1
+
+        # Find a linear combination of the prime powers from our rs
+        # with all even powers (square)
+
+        # Do gaussian elimination to get row-reduced echelon form
+        reduced, pivot_columns = row_reduce_mod_2(powers_matrix % 2)
+
+        # Components that we can choose freely
+        non_pivot_columns = set(range(L + 1)) - set(pivot_columns)
+
+        # Choose an arbitrary vector from the kernel
+        # Set the free variables to 1, so we're sure we don't get the 0-vector
+        coefficients = tuple(
+            1 if column in non_pivot_columns  # Set the free variables to 1
+            # Set the pivot columns so that each row sums to 0
+            else (sum(reduced[pivot_columns.index(column), :]) - 1) % 2
+            for column in range(L + 1)
+        )
+
+        # The product of r_j^a_j
+        x = product_mod_n(
+            (
+                pow(r, int(coefficient), n)
+                for r, coefficient in zip(r_list, coefficients, strict=True)
+            ),
+            n,
+        )
+
+        # The product of l_i^t_i
+        y = product_mod_n(
+            (
+                pow(p, int(sum(powers_matrix[prime_index, :] * coefficients)) // 2, n)
+                for prime_index, p in enumerate(primes)
+            ),
+            n,
+        )
+
+        assert pow(x, 2, n) == pow(y, 2, n)
+
+        if (x + y) % n == 0 or (x - y) % n == 0:
+            # We hit a bad pair of roots - try again
             continue
 
-        seen_rs.add(r)
+        p1 = math.gcd(x - y, n)
+        p2 = n // p1
 
-        try:
-            powers = factor_into(pow(r, 2, n), primes)
-        except FactorizationError:
-            # r^2 doesn't factor into `primes` - try again
-            continue
+        assert n % p1 == 0
 
-        r_list[r_index] = r
-        powers_matrix[:, r_index] = powers
+        if p1 > p2:
+            return (p2, p1)
 
-        r_index += 1
-
-    # Find a linear combination of the prime powers from our rs
-    # with all even powers (square)
-
-    # Do gaussian elimination to get row-reduced echelon form
-    reduced, pivot_columns = row_reduce_mod_2(powers_matrix % 2)
-
-    # Components that we can choose freely
-    non_pivot_columns = set(range(L + 1)) - set(pivot_columns)
-
-    # Choose an arbitrary vector from the kernel
-    # Set the free variables to 1, so we're sure we don't get the 0-vector
-    coefficients = tuple(
-        1 if column in non_pivot_columns  # Set the free variables to 1
-        # Set the pivot columns so that each row sums to 0
-        else (sum(reduced[pivot_columns.index(column), :]) - 1) % 2
-        for column in range(L + 1)
-    )
-
-    # The product of r_j^a_j
-    x = product_mod_n(
-        (
-            pow(r, int(coefficient), n)
-            for r, coefficient in zip(r_list, coefficients, strict=True)
-        ),
-        n,
-    )
-
-    # The product of l_i^t_i
-    y = product_mod_n(
-        (
-            pow(p, int(sum(powers_matrix[prime_index, :] * coefficients)) // 2, n)
-            for prime_index, p in enumerate(primes)
-        ),
-        n,
-    )
-
-    assert pow(x, 2, n) == pow(y, 2, n)
-
-    if (x + y) % n == 0 or (x - y) % n == 0:
-        # We hit a bad pair of roots - try again
-        return index_calculus(n, primes)
-
-    p1 = math.gcd(x - y, n)
-    p2 = n // p1
-
-    assert n % p1 == 0
-
-    if p1 > p2:
-        return (p2, p1)
-
-    return (p1, p2)
+        return (p1, p2)
